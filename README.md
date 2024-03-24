37f80db81f7835c3adc065edcc23d4a9b59722cd
James Clarke <jc1222@canterbury.ac.uk>
2024-03-24 09:52:39 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Advanced Databases And Big Data (Assignment 2)
+
+This repository is an implementation of Assignment 2 for the group project at
+Canterbury Christ Church University.
+
+# Features
+
+- Ability to Login and have users
+- Ability to create an account
+- Ability to query data from a mongodb database
+
+# Get Started
+
+Make sure rustup is installed and cargo is installed.
+
+## Commands
+
+Install Dependencies:
+
+```bash
+cargo install cargo-leptos
+```
+
+Run in Dev mode:
+
+```bash
+cd src-ui
+cargo leptos watch
+```
---
+++ src-ui/src/main.rs
@@ -0,0 +1,35 @@
+#[cfg(feature = "ssr")]
+#[tokio::main]
+async fn main() {
+    use axum::Router;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use src_ui::{app::*, fallback::file_and_error_handler};
+    use log::info;
+
+    simple_logger::init_with_level(log::Level::Info)
+        .expect("couldn't initialize logging");
+
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+    let leptos_options = conf.leptos_options;
+    let routes = generate_route_list(App);
+
+    let app = Router::new()
+        .leptos_routes(&leptos_options, routes, App)
+        .fallback(file_and_error_handler)
+        .with_state(leptos_options);
+
+    info!("listening on http://{}", &addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[cfg(not(feature = "ssr"))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for a purely client-side app
+    // see lib.rs for hydration function instead
+}
